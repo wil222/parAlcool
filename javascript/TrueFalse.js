e9cb52c6acc116911f3198c497490610060d92c2
William Andre <mrwil222@gmail.com>
2015-11-14 11:35:29 +0100

    script test1

--- javascript/TrueFalse.js
+++ javascript/TrueFalse.js
@@ -11,28 +11,47 @@ function initTF() {
 		if(value === 'true') {
 			truediv.className ='correct';
 			falsediv.className = 'incorrect';
-			truediv.className += " answered";
-			falsediv.className += " unanswered";
 		}
 		else if(value === 'semitrue') {
 			truediv.className ='semicorrect';
 			falsediv.className ='semicorrect';
-			truediv.className += " answered";
-			falsediv.className += " answered";
 		}
 		else {
 			truediv.className ='incorrect';
 			falsediv.className ='correct';
-			truediv.className += " unanswered";
-			falsediv.className += " answered";
 		}
 		container[i].appendChild(truediv);
 		container[i].appendChild(falsediv);
 	}
 }
 
-function truefalse() {
+function truefalse(id) {
 	
 }
 
+function score(id) {
+	console.log(id);
+	var optionlist = document.getElementById(id);
+	var display = document.getElementById('test1-score');
+	var value = optionlist.options[optionlist.selectedIndex].value;
+	var previous_value = optionlist.getAttribute('previous_value');
+	console.log(display.innerHTML);
+	display.innerHTML = parseInt(display.innerHTML) + parseInt(value) - parseInt(previous_value);
+	optionlist.setAttribute('previous_value', value);
+}
+
+function addTest1Listener(id) {
+	document.getElementById(id).addEventListener('click', function() {score(id);}, false);
+	document.getElementById(id).setAttribute('previous_value', 0);
+	document.getElementById(id).value = 0;
+}
+
+function initTest1() {
+	for(var i=1; i<11; i++) {
+		addTest1Listener('q1-'+i);
+	}
+}
+	
+
 initTF();
+initTest1();
