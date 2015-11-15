49bb4e85385aa15b42bb71975f3669bec9041096
William Andre <mrwil222@gmail.com>
2015-11-15 20:19:48 +0100

    comment css improvement

--- html/blog.php
+++ html/blog.php
@@ -6,61 +6,67 @@
 		</div>
 	</div>
 	<div class="container wrapper">
-	
-	<em><a href="#">Nouveau commentaire</a></em>
 
-	<?php
+		<?php
+		try {
+		    $bdd = new PDO('mysql:host=mysql1.alwaysdata.com;dbname=paralcool_comments;charset=utf8', 'paralcool', 'onvousaime');
+		}
+		catch(Exception $e) {
+			die('Erreur : '.$e->getMessage());
+		}
 
-	// Connexion à la base de données
+		$req = $bdd->query('SELECT id, titre, contenu, DATE_FORMAT(date_creation, \'%d/%m/%Y à %Hh%imin%ss\') AS date_creation_fr FROM billets ORDER BY date_creation DESC LIMIT 0, 5');
+		$req2 = $bdd->prepare('SELECT auteur, commentaire, DATE_FORMAT(date_commentaire, \'%d/%m/%Y à %Hh%imin%ss\') AS date_commentaire_fr FROM commentaires WHERE id_billet = ? ORDER BY date_commentaire');
 
-	try
-	{
-	    $bdd = new PDO('mysql:host=mysql1.alwaysdata.com;dbname=paralcool_comments;charset=utf8', 'paralcool', 'onvousaime');
-	}
-	catch(Exception $e)
-	{
-		die('Erreur : '.$e->getMessage());
-	}
+		while ($donnees = $req->fetch())
+		{
+		?>
+		<div class="comment-container">
+			<h3><?php echo htmlspecialchars($donnees['titre']); ?> <span class="date">le <?php echo $donnees['date_creation_fr']; ?></span></h3>
+		    
+			<p>
+				<?php echo nl2br(htmlspecialchars($donnees['contenu']));?>
+			<br />
+				<?php
+					$req2->execute(array($donnees['id']));
 
-	// On récupère les 5 derniers billets
-	$req = $bdd->query('SELECT id, titre, contenu, DATE_FORMAT(date_creation, \'%d/%m/%Y à %Hh%imin%ss\') AS date_creation_fr FROM billets ORDER BY date_creation DESC LIMIT 0, 5');
-
-	while ($donnees = $req->fetch())
-	{
-	?>
-	<div class="news">
-	    <h3>
-		<?php echo htmlspecialchars($donnees['titre']); ?>
-	    </h3>
-	    
-		<em>le <?php echo $donnees['date_creation_fr']; ?></em>
-	    
-	    <p>
-		    <?php
-		    // On affiche le contenu du billet
-		    echo nl2br(htmlspecialchars($donnees['contenu']));
-		    ?>
-		    <br />
-			<?php
-				// Récupération des réponses
-				$req2 = $bdd->prepare('SELECT auteur, commentaire, DATE_FORMAT(date_commentaire, \'%d/%m/%Y à %Hh%imin%ss\') AS date_commentaire_fr FROM commentaires WHERE id_billet = ? ORDER BY date_commentaire');
-				$req2->execute(array($donnees['id']));
-
-				while ($donnees2 = $req2->fetch())
-				{
-			?>
-					<p><strong><?php echo htmlspecialchars($donnees2['auteur']); ?></strong> le <?php echo $donnees2['date_commentaire_fr']; ?></p>
-					<p><?php echo nl2br(htmlspecialchars($donnees2['commentaire'])); ?></p>
-			<?php
-				} // Fin de la boucle des réponses
-				$req2->closeCursor();
-			?>
-		    <em><a href="#">Répondre</a></em>
-	    </p>
-	</div>
-	<?php
-	} // Fin de la boucle des billets
-	$req->closeCursor();
-	?>
+					while ($donnees2 = $req2->fetch())
+					{
+				?>
+						<div class="comment-answer">
+							<p><span class="author"><?php echo htmlspecialchars($donnees2['auteur']); ?></span> <span class="date">le <?php echo $donnees2['date_commentaire_fr']; ?></span></p>
+							<p><?php echo nl2br(htmlspecialchars($donnees2['commentaire'])); ?></p>
+						</div>
+				<?php
+					}
+				?>
+				<form id="answer-<?php echo $donnees['id'];?>">
+					<a type="submit" href="#">Répondre</a>
+					<div class="comment-area" hidden>
+						<legend>Votre commentaire :</legend>
+						<div><textarea placeholder="Commentaire" required></textarea></div>
+						<input type="text" placeholder="Pseudo" required/>
+						<input type="submit" value="Envoyer"/>
+						<label><input name="conditions" type="checkbox" /> J'ai lu et j'accepte les <a href="#">conditions d'utilisation</a></label>
+					</div>
+				</form>
+			</p>
+		</div>
+		<?php
+		}
+		$req2->closeCursor();
+		$req->closeCursor();
+		?>
+		
+		<form id="newcomment">
+			<input type="submit" class="btn btn-primary" value="Nouveau commentaire"/>
+			<div class="comment-area" hidden>
+				<h2>Votre commentaire :</h2>
+				<div><textarea placeholder="Commentaire" required></textarea></div>
+				<input type="text" placeholder="Pseudo" required/>
+				<input type="submit" value="Envoyer"/>
+				<label><input name="conditions" type="checkbox" /> J'ai lu et j'accepte les <a href="#">conditions d'utilisation</a></label>
+			</div>
+		</form>
 	</div>
 </section>
